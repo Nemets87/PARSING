586fecdc41ded0dd3ef945e92ebb92dc7e9b39be
Nemets87 <bonustime161@yandex.ru>
2023-12-15 22:50:13 +0300

    phind_update

--- PARSING1/nemets87/nemets87/swapi.py
+++ PARSING1/nemets87/nemets87/swapi.py
@@ -1,35 +1,55 @@
 from bs4 import BeautifulSoup
-
 import time
 import telebot
 import requests
 import os
 import dotenv
 
-
+# Load environment variables
 dotenv.load_dotenv()
 
-
+# Constants
 TOKEN = os.getenv('TOKEN')
-bot = telebot.TeleBot(TOKEN)
 CHAT_ID = os.getenv('CHAT_ID')
+URL = 'https://joystick161.ru/products/55959059'
+SLEEP_INTERVAL = 60*10  # 10 minutes
 
-url = 'https://joystick161.ru/products/55959059'
-previous_price = ''
+# Initialize bot
+bot = telebot.TeleBot(TOKEN)
 
 
-def check_price():
-    global previous_price
-    r = requests.get(url)  
-    soup = BeautifulSoup(r.text, 'lxml')  
-    new_price = soup.find(
-        'span', {'class': 'product-price-data'}).get('data-cost'
-                                                     )
+def check_price(previous_price):
+    """
+    Check the price of a product and send a message if it has changed.
+    """
+    try:
+        r = requests.get(URL)  
+        soup = BeautifulSoup(r.text, 'lxml')  
+        new_price = soup.find('span', {'class': 'product-price-data'}).get('data-cost')
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return previous_price
+
     if previous_price != new_price:
-        bot.send_message(CHAT_ID, 'Price changed: ' + new_price)
-        previous_price = new_price
+        try:
+            bot.send_message(CHAT_ID, 'Price changed: ' + new_price)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        else:
+            previous_price = new_price
+
+    return previous_price
 
 
-while True:
-    check_price()
-    time.sleep(60*10)
+def main():
+    """
+    Main function that checks the price every 10 minutes.
+    """
+    previous_price = ''
+    while True:
+        previous_price = check_price(previous_price)
+        time.sleep(SLEEP_INTERVAL)
+
+
+if __name__ == "__main__":
+    main()
